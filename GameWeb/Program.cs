e4fb5375c8cca01d5059f256b9499c4a918644b4
edu4pravin <edu4pravin@gmail.com>
2019-12-12 23:12:58 -0500

    Test 001

--- GameWeb/Program.cs
+++ GameWeb/Program.cs
@@ -15,7 +15,7 @@ namespace GameWeb
         public static void Main(string[] args)
         {
             //This is one more test
-            Console.Write("Test");
+            Console.Write("Test 001");
             CreateWebHostBuilder(args).Build().Run();
         }
 
